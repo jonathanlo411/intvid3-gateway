d4a0e352aefc4da9f6270a73c8fcaf8e6d7e110e
Jonathan Lo <jonathanlo411@gmail.com>
2023-09-05 21:10:06 -0700

    trim fat

---
+++ src/app/sub/page.tsx
@@ -0,0 +1,10 @@
+
+export default function SubTree() {
+    return (
+        <div>
+            <h1>Hello world from sub</h1>
+            <p>This is an edit</p>
+            <a href="/">This is link to home</a>
+        </div>
+    )
+}
